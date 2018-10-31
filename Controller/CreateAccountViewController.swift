7540dbf46e3c72cebd2c516053fb310759d421df
Pete Kreppein <pmkreppein@gmail.com>
2018-10-31 17:22:48 -0400

    Create Account Screen

---
+++ Controller/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  Smack
+//
+//  Created by Peter M Kreppein on 10/31/18.
+//  Copyright © 2018 Peter M Kreppein. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
