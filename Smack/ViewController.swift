57322acb6daf4903e56dd04886efc8bd38c5ef41
Pete Kreppein <pmkreppein@gmail.com>
2018-10-31 12:51:10 -0400

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Peter M Kreppein on 10/31/18.
+//  Copyright © 2018 Peter M Kreppein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
