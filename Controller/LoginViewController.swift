6fbc4b06ceef49fe58da2f35c0db5759801f5560
Pete Kreppein <pmkreppein@gmail.com>
2018-10-31 16:49:04 -0400

    Configed LoginVC and Storyboard

---
+++ Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Smack
+//
+//  Created by Peter M Kreppein on 10/31/18.
+//  Copyright © 2018 Peter M Kreppein. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    @IBAction func didPressCloseButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Peter M Kreppein on 10/31/18.
+//  Copyright © 2018 Peter M Kreppein. All rights reserved.
+//
+
+import Foundation
+let TO_LOGIN = "toLoginScreenSegue"
